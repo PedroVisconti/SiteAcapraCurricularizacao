17c00d212c08671dbf5c3d20c514bc8c3f92a373
Iago Geovane Salvador <75733383+iagslvdr@users.noreply.github.com>
2025-08-27 09:45:50 -0300

    fix: descomentado inicializacao banco

--- SiteAcapra/Program.cs
+++ SiteAcapra/Program.cs
@@ -45,7 +45,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    //dbContext.Database.Migrate();
+    dbContext.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
