78a0e7517507578fff94b1788ed9f353ad41feb7
PedroVisconti <pedroarthurvisconti@hotmail.com>
2025-08-27 21:51:14 -0300

    CM - Configuracao AnimalController

--- SiteAcapra/Mappings/MappingProfile.cs
+++ SiteAcapra/Mappings/MappingProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using SiteAcapra.DTOs.AnimalDTO;
 using SiteAcapra.DTOs.Requests;
 using SiteAcapra.DTOs.Responses;
 using SiteAcapra.Models;
@@ -9,8 +10,19 @@ namespace SiteAcapra.Mappings
     {
         public MappingProfile()
         {
-            CreateMap<Usuario, UsuarioResponse>();
+            // primeiro qual sera a saida e em segundo oque vai ser usado como paramentro para criar
+
+            CreateMap<Usuario, UsuarioResponse>(); 
             CreateMap<RegisterRequest, Usuario>();
+            CreateMap<Vacina, AnimalVacinaDTO>()
+                .ForMember(dest => dest.VacinaNome, opt => opt.MapFrom(src => src.Nome))
+                .ForMember(dest => dest.DataVacina, opt => opt.MapFrom(src => src.DataAplicacao));
+            CreateMap<Animal, AnimalResponse>()
+                .ForMember(dest => dest.AnimalVacinas, opt => opt.MapFrom(src => src.AnimalVacinas))
+                .ForMember(dest => dest.Raca, opt => opt.MapFrom(src => src.Raca))
+                .ForMember(dest => dest.Especie, opt => opt.MapFrom(src => src.Especie))
+                .ForMember(dest => dest.Tutor, opt => opt.MapFrom(src => src.Tutor))
+                .ForMember(dest => dest.Fotos, opt => opt.MapFrom(src => src.Fotos));
         }
     }
 }
--- SiteAcapra/Models/Vacina.cs
+++ SiteAcapra/Models/Vacina.cs
@@ -4,6 +4,7 @@
     {
         public int VacinaId { get; set; }
         public string Nome { get; set; }
+        public DateOnly? DataAplicacao { get; set; }
         public bool Excluido { get; set; }
         public ICollection<AnimalVacina> AnimalVacinas { get; set; } = new List<AnimalVacina>();
     }
