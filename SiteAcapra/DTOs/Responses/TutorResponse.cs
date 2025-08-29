29e0f8850f8d9ec5581ca91f16858edbb9eb7a2d
PedroVisconti <pedroarthurvisconti@hotmail.com>
2025-08-29 18:33:38 -0300

    CM - Configurado o TutorController

---
+++ SiteAcapra/DTOs/Responses/TutorResponse.cs
@@ -0,0 +1,17 @@
+﻿using SiteAcapra.Models;
+
+namespace SiteAcapra.DTOs.Responses
+{
+    public class TutorResponse
+    {
+        public Guid TutorId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public char? Sexo { get; set; }
+    }
+}
