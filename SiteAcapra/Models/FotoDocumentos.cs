d9defa5141fdddbdbeb359fd2a6a0b8db42ee9b4
jbecker <becker6667@gmail.com>
2025-08-28 21:46:24 -0300

    CM - Ajuste no formulario

---
+++ SiteAcapra/Models/FotoDocumentos.cs
@@ -0,0 +1,11 @@
+﻿namespace SiteAcapra.Models
+{
+    public class FotoDocumentos
+    {
+        public Guid FotoId { get; set; }
+        public string FotoHash { get; set; }
+        public bool Excluido { get; set; }
+        public int FormularioId { get; set; }
+        public FormularioAdocao Formulario { get; set; }
+    }
+}
